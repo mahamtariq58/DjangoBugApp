cd651b7b7122bba98dc826f8044b8707ec64faf0
Maham Tariq <mahamtariq58@gmail.com>
2023-10-03 17:44:23 +0000

    Register the bug model

--- bug/admin.py
+++ bug/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from bug.models import Bug
 # Register your models here.
+class BugAdmin(admin.ModelAdmin):
+    list_display = ('bug_description','bug_type','report_date','status')
+
+
+
+admin.site.register(Bug,BugAdmin)
+    
