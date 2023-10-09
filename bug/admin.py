a16d83455704cc1260d9477e14e307efed61ed31
Maham Tariq <mahamtariq58@gmail.com>
2023-10-10 02:50:37 +0500

    Create & register the bug model

--- bug/admin.py
+++ bug/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Bug
 
-# Register your models here.
+class BugAdmin(admin.ModelAdmin):
+    list_display = ('bug_description','bug_type','report_date','status')
+
+admin.site.register(Bug,BugAdmin)
--- bug/models.py
+++ bug/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+class Bug(models.Model):
+    
+    # Tuple for bug_types
+    BUG_TYPES = (
+        ('error', 'error'), 
+        ('new feature','new feature'), 
+        ('code','code'),
+        ('enhancement','enhancement')
+    )
+    STATUS = (
+        ('To do','To do'),
+        ('In Progress','In Progress'),
+        ('Done','Done')
+    )
+
+    bug_description = models.TextField()
+    bug_type = models.CharField(max_length = 50, choices = BUG_TYPES)
+    report_date = models.DateField()
+    status = models.CharField(max_length = 50, choices = STATUS)
+    
+    # string representation
+    def __str__(self):
+        return f"{self.bug_description} ({self.bug_type} - Reported on {self.report_date.strftime('%b. %d, %Y')})"
+    
